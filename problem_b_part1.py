8af9562af8663ef6fa2bee11d314c5b4bc387a70
Rabih Kodeih <rabih.kodeih@gmail.com>
2018-03-07 15:53:13 +0200

    Finish second problem

---
+++ problem_b_part1.py
@@ -0,0 +1,140 @@
+'''
+Created on Mar 6, 2018
+
+@author: rabihkodeih
+'''
+
+import os
+import sys
+import csv
+from datetime import datetime
+import multiprocessing as mp
+import ujson  # @UnresolvedImport
+from functools import wraps
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_PATH = os.path.join(BASE_DIR, 'data')
+LIMIT = 0
+
+data_map = {}
+
+
+def get_median(data):
+    n = len(data)
+    if n == 0:
+        return 0
+    if n % 2 == 1:
+        return data[n//2]
+    else:
+        i = n // 2
+        return (data[i - 1] + data[i])/2
+
+
+def measure_time(func):
+    @wraps(func)
+    def wrapped(*args, **kwargs):
+        t_start = datetime.now()
+        result = func(*args, **kwargs)
+        t_end = datetime.now()
+        sys.stdout.write('Operation completed in %s seconds\n' % (t_end - t_start))
+        return result
+    return wrapped
+
+
+def worker(jsonline):
+    data = ujson.loads(jsonline)
+    target_id = data['target']['id']
+    disease_id = data['disease']['id']
+    association_score = data['scores']['association_score']
+    return (target_id, disease_id, association_score) 
+    
+    
+def process_result(result):
+    for target_id, disease_id, association_score in result:
+        if target_id not in data_map:
+            data_map[target_id] = {}
+        if disease_id not in data_map[target_id]:
+            data_map[target_id][disease_id] = []
+        data_map[target_id][disease_id].append(association_score)
+
+
+@measure_time
+def handle_main_single(data_path):
+    with open(data_path, 'rb') as jsonsrc:
+        for ith, jsonline in enumerate(jsonsrc):
+            if ith % 100000 == 0: sys.stdout.write('read %s data lines\n' % ith)
+            data = ujson.loads(jsonline)
+            target_id = data['target']['id']
+            disease_id = data['disease']['id']
+            association_score = data['scores']['association_score']
+            if target_id not in data_map:
+                data_map[target_id] = {}
+            if disease_id not in data_map[target_id]:
+                data_map[target_id][disease_id] = []
+            data_map[target_id][disease_id].append(association_score)
+            if ith == LIMIT - 1: break
+
+
+@measure_time
+def handle_main_multi(data_path):
+    pool = mp.Pool()
+    with open(data_path, 'rb') as jsonsrc:
+        partial = []
+        for ith, line in enumerate(jsonsrc):
+            if ith % 100000 == 0: sys.stdout.write('read %s data lines\n' % ith)
+            partial.append(line)
+            if len(partial) == 5000:
+                process_result(pool.map(worker, partial))
+                partial = []
+            if ith == LIMIT - 1: break 
+        process_result(pool.map(worker, partial))
+        pool.close()
+        pool.join()
+
+
+@measure_time
+def output_stage_processing(data_map, output_path):
+    big_list = []
+    for target_id, v in data_map.items():
+        for disease_id, scores in v.items():
+            scores.sort()
+            ln = len(scores)
+            if ln <= 3:
+                top3 = scores
+            else:
+                top3 = scores[ln - 3:ln]
+            median = get_median(scores)
+            big_list.append((median, target_id, disease_id, top3))
+    big_list.sort()
+    write_path = os.path.join(output_path, 'problem_b_first_part.csv')
+    with open(write_path, 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        for median, target_id, disease_id, top3 in big_list:
+            row = [target_id, disease_id, median]
+            row.extend(top3)
+            writer.writerow(row)
+
+
+if __name__ == '__main__':
+    sys.stdout.write('starting...\n\n')
+    
+    data_path = os.path.join(DATA_PATH, 'input', '17.12_evidence_data.json')
+    output_path = os.path.join(DATA_PATH, 'output')
+    assert os.path.exists(output_path)
+        
+    handle_main_multi(data_path)
+    output_stage_processing(data_map, output_path)
+    
+    sys.stdout.write('\n\ndone')
+
+    #Operation completed in 0:03:34.487030 seconds
+    #Operation completed in 0:00:12.608518 seconds
+
+
+
+
+
+
+
+
---
+++ problem_b_part2.py
@@ -0,0 +1,208 @@
+'''
+Created on Mar 6, 2018
+
+@author: rabihkodeih
+'''
+
+import os
+import sys
+import csv
+import multiprocessing as mp
+from datetime import datetime
+from functools import wraps
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_PATH = os.path.join(BASE_DIR, 'data')
+LIMIT = -1
+
+data_map = {}
+
+
+def get_median(data):
+    n = len(data)
+    if n == 0:
+        return 0
+    if n % 2 == 1:
+        return data[n//2]
+    else:
+        i = n // 2
+        return (data[i - 1] + data[i])/2
+
+
+def measure_time(func):
+    @wraps(func)
+    def wrapped(*args, **kwargs):
+        t_start = datetime.now()
+        result = func(*args, **kwargs)
+        t_end = datetime.now()
+        sys.stdout.write('Operation completed in %s seconds\n' % (t_end - t_start))
+        return result
+    return wrapped
+
+
+@measure_time
+def get_data_map(data_path):
+    data_map = {}
+    with open(data_path) as csvfile:
+        reader = csv.reader(csvfile)
+        for ith, row in enumerate(reader):
+            if ith % 100000 == 0:
+                sys.stdout.write('data_map: processed %s rows\n' % ith)
+            target_id, disease_id = row[0], row[1]
+            if target_id not in data_map: data_map[target_id] = []
+            data_map[target_id].append(disease_id)
+            if ith == LIMIT: break
+    return data_map
+
+
+@measure_time
+def count_single(data_map):
+    pair_count = 0
+    total = (len(data_map) * (len(data_map) - 1)) / 2000000
+    items = list(data_map.values())
+    row_count = 0
+    for ith, v1 in enumerate(items[:-1]):
+        for v2 in items[ith + 1:]:
+            if  len(set(v1).intersection(set(v2))) > 1:
+                pair_count += 1
+            if row_count % 1000000 == 0:
+                sys.stdout.write('count_single: processed %s M rows out of %s Ms, (%s)\n' % (row_count/1000000, total, pair_count))
+            row_count += 1
+    return pair_count
+    
+    
+def worker(pairs):
+    count = 0
+    for v1, v2 in pairs:
+        v = len(set(v1).intersection(set(v2)))
+        if v > 1:
+            count += 1
+    return count
+    
+
+@measure_time
+def count_multi(data_map):
+    num_chunks = mp.cpu_count() - 1
+    chunk_size = 1000
+    pair_count = 0
+    total = (len(data_map) * (len(data_map) - 1)) / 2000000
+    data = []
+    chunk = []
+    items = list(data_map.values())
+    pool = mp.Pool()
+    row_count = 0
+    for ith, target1 in enumerate(items[:-1]):
+        for target2 in items[ith + 1:]:
+            chunk.append((target1, target2))
+            if ith % (chunk_size) == chunk_size - 1:
+                data.append(chunk)
+                chunk = []
+                if len(data) == num_chunks:
+                    pair_count += sum(pool.map(worker, data))
+                    data = []
+            if row_count % 500000 == 0:
+                sys.stdout.write('count_single: processed %s M rows out of %s Ms, (%s)\n' % (row_count/1000000, total, pair_count))
+            row_count += 1
+    if chunk: data.append(chunk)
+    pair_count += sum(pool.map(worker, data))
+    pool.close()
+    pool.join()
+    return pair_count
+
+
+
+def worker2(pair):
+    v1, v2 = pair
+    v = len(set(v1).intersection(set(v2)))
+    if v > 1:
+        return 1
+    return 0
+
+
+@measure_time
+def count_multi2(data_map):
+    pool = mp.Pool()
+    pair_count = 0
+    total = (len(data_map) * (len(data_map) - 1)) / 2000000
+    items = list(data_map.values())
+    chunk = []
+    row_count = 0
+    for ith, v1 in enumerate(items[:-1]):
+        for v2 in items[ith + 1:]:
+            chunk.append((v1, v2))
+            if len(chunk) == 10000:
+                pair_count += sum(pool.map(worker2, chunk))
+                chunk = []
+            if row_count % 500000 == 0:
+                sys.stdout.write('count_single: processed %s M rows out of %s Ms, (%s)\n' % (row_count/1000000, total, pair_count))
+            row_count += 1
+    pair_count += sum(pool.map(worker2, chunk))
+    pool.close()
+    pool.join()
+    return pair_count
+
+
+@measure_time
+def create_jobs(data_map, jobs_path, num_jobs):
+    import shutil
+    if os.path.exists(jobs_path):
+        shutil.rmtree(jobs_path)
+    os.makedirs(jobs_path)
+    items = list(data_map.values())
+    total = (len(data_map) * (len(data_map) - 1)) // 2
+    chunk_size = total // num_jobs
+    job_count = 0
+    row_count = 0
+    write_path = os.path.join(jobs_path, 'jobs_%s.csv' % job_count)
+    job = open(write_path, 'w') 
+    writer = csv.writer(job)
+    sys.stdout.write('inintialized job %s\n' % job_count)
+    for ith, v1 in enumerate(items[:-1]):
+        for v2 in items[ith + 1:]:
+            if row_count == chunk_size:
+                row_count = 0
+                job.close()
+                sys.stdout.write('created job %s\n' % job_count)
+                job_count += 1
+                write_path = os.path.join(jobs_path, 'jobs_%s.csv' % job_count)
+                job = open(write_path, 'w') 
+                writer = csv.writer(job)
+                sys.stdout.write('initialized job %s\n' % job_count)
+            row_count += 1
+            writer.writerow(v1)
+            writer.writerow(v2)
+    job.close()
+    sys.stdout.write('created job %s\n' % job_count)
+                
+    
+
+if __name__ == '__main__':
+    sys.stdout.write('starting...\n\n')
+    
+    
+    data_path = os.path.join(DATA_PATH, 'output', 'problem_b_first_part.csv')
+    jobs_path = os.path.join(DATA_PATH, 'output', 'jobs')
+    assert os.path.exists(data_path)
+    
+    data_map = get_data_map(data_path)
+    result = count_single(data_map)
+    #result = count_multi(data_map)
+    #result = count_multi2(data_map)
+    sys.stdout.write('%s\n' % result)
+    
+    #Operation completed in 0:00:02.551176 seconds
+    #Operation completed in 0:43:12.404540 seconds
+    #121114622
+    
+    sys.stdout.write('\n\ndone')
+
+
+
+
+
+
+
+
+
+
